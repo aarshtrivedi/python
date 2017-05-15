cd649446113d85fdbc8fff35f44d3db73d29d1cf
aarshtrivedi <aarsht@yahoo.com>
2017-05-15 02:25:17 -0400

    Add files via upload

---
+++ Absolute Value.py
@@ -0,0 +1,13 @@
+# Determining the absolute value of a number
+
+import math
+sqrt = math.sqrt
+
+
+number = float(input("What is the number? "))
+
+print("The absolute value of", number, "is", sqrt((number ** 2)))
+
+print("Press ENTER to exit the program")
+
+
